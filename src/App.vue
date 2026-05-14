274577b066c2654f5daa8e4a0931a81974ffba38
kirbyeater23 <sandra.martinez97@educa.madrid.org>
2026-05-14 16:29:10 +0200

    borrado general

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Button from './components/Button.vue'
-</script>
-
-<template>
-  <!-- <HelloWorld />-->
-  <Button />
-</template>
